e580ed8f2e99bec5b49c46157c086839acde829b
beanmanthree <ivanchang30901@gmail.com>
2025-11-27 23:27:22 -0700

    Second

--- main.cpp
+++ main.cpp
@@ -9,10 +9,11 @@ using namespace sf;
 using namespace std;
 
 int main() {
-    RenderWindow window(VideoMode(800, 600), "beanmanthree");
+    RenderWindow window(VideoMode(1280, 720), "beanmanthree");
     window.setVerticalSyncEnabled(false);
     window.setFramerateLimit(60);
     CircleShape shape(100.f);
+    shape.setPosition(540.f, 260.f);
     shape.setFillColor(Color::Green);
     while (window.isOpen()) {
         Event event;
